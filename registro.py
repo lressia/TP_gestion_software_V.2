46b56fa80cf898b1b8ec92a89ab4fbae9f779c1f
Marcos111111 <106403146+Marcos111111@users.noreply.github.com>
2022-09-28 15:57:39 -0300

    Update registro.py

--- registro.py
+++ registro.py
@@ -1,1 +1,20 @@
+class Software:
+    def __init__(self, nombre_usuario="", repositorio="", descripcion="", fecha="", leng="", likes="", tags=(), url=""):
+        self.nombre_usuario = nombre_usuario
+        self.repositorio = repositorio
+        self.descripcion = descripcion
+        self.fecha_actualizacion = fecha
+        self.lenguaje = leng
+        self.likes = likes
+        self.tags = tags
+        self.url = url
+
+
+def to_string(software):
+    res = "Nombre de usuario: "+str(software.nombre_usuario)+" | Repositorio: "+str(software.repositorio) +\
+          " | Descripcion: "+software.descripcion +\
+          " | Fecha de actualizacion: "+software.fecha_actualizacion+" | Lenguaje: "+software.lenguaje +\
+          " | Likes: "+str(software.likes) + " | Tags: "+str(software.tags)+" | URL: "+str(software.url)
+
+    return res
 
