c3f21dfcd5d8c0d4638d604e849cc9905c2ad5c8
Marcos111111 <106403146+Marcos111111@users.noreply.github.com>
2022-09-30 02:30:28 -0300

    Update registro.py

--- registro.py
+++ registro.py
@@ -1,4 +1,4 @@
-class proyecto:
+class Proyecto:
     def __init__(self, nombre_usuario="", repositorio="", fecha="", leng="", likes=float(0), tags=(), url=""):
         self.nombre_usuario = nombre_usuario
         self.repositorio = repositorio
@@ -12,8 +12,24 @@ class proyecto:
 def to_string(software):
     res = "Nombre de usuario: "+str(software.nombre_usuario)+" | Repositorio: "+str(software.repositorio) +\
           " | Fecha de actualizacion: "+software.fecha_actualizacion+" | Lenguaje: "+software.lenguaje +\
-          " | Likes: "+str(software.likes)+"K"+" | Tags: "+str(software.tags)+" | URL: "+str(software.url)
+          " | Likes: "+str(software.likes)+" | Tags: "+str(software.tags)+" | URL: "+str(software.url)
 
     return res
 
 
+def conver_estrellas(software):
+    rangos = (0, 10, 20, 30, 40)
+    estrellas = 0
+    for i in range(len(rangos)):
+        if software.likes <= rangos[i] and software.likes > rangos[i-1]:
+            estrellas = i
+
+    return estrellas
+
+
+def to_string_acotado(software):
+    estrellas = conver_estrellas(software)
+    res = "Repositorio: "+str(software.repositorio) +\
+          " | Fecha de actualizacion: "+software.fecha_actualizacion+" | Estrellas: "+str(estrellas)
+
+    return res
